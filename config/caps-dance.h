962a718ef117536dab7080e147d8a317807ee431
Paul Mourer <paul.mourer@onxmaps.com>
2024-12-06 22:11:15 -0700

    No macro def

--- config/caps-dance.h
+++
@@ -1,8 +0,0 @@
-// urob helpers
-#include <zmk-helpers/helper.h>
-#include <zmk-helpers/key-labels/lily58.h>
-
-ZMK_TAP_DANCE(capsdance, 
-  tapping-term-ms = <200>;
-  bindings = <&kp LSHIFT>, <&caps_word>, <&kp CAPS_LOCK>;
-)
