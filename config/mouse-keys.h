cc0defd819d3f365d3142415e8962ad0de5ad828
Paul Mourer <paul.mourer@onxmaps.com>
2024-12-08 16:10:33 -0700

    tweak again

--- config/mouse-keys.h
+++ config/mouse-keys.h
@@ -9,7 +9,7 @@
  */
  
 // Default 600
-#define ZMK_MOUSE_DEFAULT_MOVE_VAL 3800
+#define ZMK_MOUSE_DEFAULT_MOVE_VAL 4900
 // Default 10
 #define ZMK_MOUSE_DEFAULT_SCRL_VAL 15
 
@@ -26,7 +26,7 @@
 
 &mmv {
   acceleration-exponent = <2>;
-  time-to-max-speed-ms = <2000>;
+  time-to-max-speed-ms = <2500>;
   delay-ms = <0>;
   trigger-period-ms = <2>;
 };
@@ -34,7 +34,7 @@
 
 &msc {
   acceleration-exponent = <2>;
-  time-to-max-speed-ms = <3000>;
+  time-to-max-speed-ms = <4000>;
   delay-ms = <0>;
   trigger-period-ms = <2>;
 };
