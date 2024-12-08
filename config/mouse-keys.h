cb6025e0f2c9a0198c8edeba70b63f95a9445866
Paul Mourer <paul.mourer@onxmaps.com>
2024-12-07 17:10:27 -0700

    Don't redefine internal ZMK values

--- config/mouse-keys.h
+++ config/mouse-keys.h
@@ -5,8 +5,6 @@
  * https://github.com/manna-harbour/miryoku_zmk/blob/master/miryoku/miryoku_kludge_mousekeyspr.dtsi
  * https://github.com/manna-harbour/miryoku_zmk/blob/master/miryoku/miryoku_kludge_mousekeyspr.h
  */
-#define ZMK_MOUSE_DEFAULT_MOVE_VAL 1250
-#define ZMK_MOUSE_DEFAULT_SCRL_VAL 100
 
 #define U_MOUSE_MOVE_EXPONENT 1
 #define U_MOUSE_MOVE_TIME 1500
