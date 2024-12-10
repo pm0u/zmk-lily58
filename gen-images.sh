bbb79a8431d78c2947eb804fbc5c7eb1073a033c
Paul Mourer <paul.mourer@onxmaps.com>
2024-12-09 22:34:45 -0700

    update image, update some keycodes

--- gen-images.sh
+++ gen-images.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # zmk-viewer generate -f config/lily58.keymap lily58/rev1
-keymap parse -c 14 -z ./config/lily58.keymap > lily.yml
-keymap draw lily.yml > lily.svg
+keymap -c ./keymap_config.yml parse -c 14 -z ./config/lily58.keymap > lily.yml
+keymap -c ./keymap_config.yml draw lily.yml > lily.svg
 rm lily.yml
