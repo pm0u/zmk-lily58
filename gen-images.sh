004891fd0a64e0e2f8903752c16044ebeefb4bf5
Paul Mourer <paul.mourer@onxmaps.com>
2024-12-11 19:56:55 -0700

    Tweaks to combo timing

--- gen-images.sh
+++ gen-images.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # zmk-viewer generate -f config/lily58.keymap lily58/rev1
-keymap -c ./keymap_config.yml parse -c 14 -z ./config/lily58.keymap > lily.yml
-keymap -c ./keymap_config.yml draw lily.yml > lily.svg
+keymap -c ./keymap_config.yml parse -z ./config/lily58.keymap > lily.yml
+keymap -c ./keymap_config.yml draw lily.yml -k "lily58/rev1" > lily.svg
 rm lily.yml
